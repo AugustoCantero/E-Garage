d669a9dd50c53efb6149290986676d6b3737ac58
AugustoCantero <augustocantero2002@gmail.com>
2024-11-11 16:55:44 -0300

    implementacion notificaciones push

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/router/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/preferencias/pref_usuarios.dart';
+import 'package:flutter_application_1/services/bloc/notifications_bloc.dart';
+import 'package:flutter_application_1/services/localNotification/local_notification.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await PreferenciasUsuario.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(ProviderScope(child: EGarajeApp()));
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  await LocalNotification.initializeLocalNotifications();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NotificationsBloc()),
+      ],
+      child: ProviderScope(child: EGarajeApp()),
+    )
+  );
 }
 
 class EGarajeApp extends StatelessWidget {
