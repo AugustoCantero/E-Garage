8e919bd0eeb74db280d8f5c3c560c91e53e238b8
Alejandro Agustin Collado <alejandro.agustincollado@gmail.com>
2024-11-16 23:08:26 -0300

    Arreglo para que se puedan eliminar reservas

--- lib/Test/testModificacionReserva.dart
+++ lib/Test/testModificacionReserva.dart
@@ -2,7 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/Entities/Reserva.dart';
 import 'package:flutter_application_1/core/Providers/reservaGarage.dart';
+import 'package:flutter_application_1/core/router/app_router.dart';
+import 'package:flutter_application_1/screens/LoginUsuario.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class editarReserva extends ConsumerWidget {
   static final String name = 'editarReserva';
@@ -29,8 +32,10 @@ class editarReserva extends ConsumerWidget {
           const SizedBox(height: 20),
           Center(
             child: ElevatedButton(
-              onPressed:
-                  _cancelarReserva, // Aquí llamas a la función _cancelarReserva
+              onPressed: () async {
+                await _cancelarReserva();
+                context.goNamed(LoginUsuario.name);
+              }, // Aquí llamas a la función _cancelarReserva
               child: const Text("Cancelar Reserva"),
             ),
           ),
